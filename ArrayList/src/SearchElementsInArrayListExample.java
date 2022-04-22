848844936c267fd5153cd83849da70a0f8f8d295
Duy Linh <cudotn111@gmail.com>
2022-04-23 01:18:23 +0700

    DL

---
+++ ArrayList/src/SearchElementsInArrayListExample.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+import java.util.List;
+public class SearchElementsInArrayListExample {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("John");
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Steve");
+        names.add("John");
+        names.add("Steve");
+        names.add("Maria");
+
+        // Check if an ArrayList contains a given element
+        // Kiểm tra xem ArrayList có chứa một phần tử đã cho hay không
+        System.out.println("indexOf\"Steve\": "  + names.indexOf("Steve"));
+        System.out.println("indexOf \"Mark\": " + names.indexOf("Mark"));
+
+        // Find the index of the last occurrence of an element in an ArrayList
+        System.out.println("lastIndexOf \"John\" : " + names.lastIndexOf("John"));
+        System.out.println("lastIndexOf \"Bill\" : " + names.lastIndexOf("Bill"));
+    }
+}
---
+++ ArrayList/src/studentManagement.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class studentManagement {
+       private String RollNo;
+       private String FullName;
+       private int DOB;
+       private String Address;
+       private String Email;
+       private String  Status;
+
+
+}
